f5657589e156697e33a8b9652184e1b3c803102f
Daniil11ru <daniilmiroshnik11062001@gmail.com>
2024-09-17 02:25:08 +0300

    Add dummy buttons for managing agents

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Pagination, Grid2 } from "@mui/material";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
@@ -7,11 +7,12 @@ import IconButton from "@mui/material/IconButton";
 import SearchField from "./components/material/SearchField";
 import Dropdown from "./components/material/Dropdown";
 import AgentCard from "./components/material/AgentCardVertical";
+import Button from "./components/material/Button";
 
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 
-import { ArrowDownward, ArrowUpward } from "@mui/icons-material";
+import { ArrowDownward, ArrowUpward, More } from "@mui/icons-material";
 
 import { AgentKey } from "./data/AgentSchema";
 import { SortOptions } from "./data/SortOptions";
@@ -33,7 +34,7 @@ const App: React.FC = () => {
   const [selectedType, setSelectedType] = useState<string>("Все");
   const [sortCriteria, setSortCriteria] = useState<string>(AgentKey.name);
   const [sortOrder, setSortOrder] = useState<SortOptions>(SortOptions.ASC);
-  const [selectedAgents, setSelectedAgents] = useState<string[]>([]);
+  const [selectedAgents, setSelectedAgents] = useState<number[]>([]);
 
   const agentLocalSource = new AgentLocalSource();
   const agentRepository = new AgentRepository(agentLocalSource);
@@ -50,7 +51,9 @@ const App: React.FC = () => {
       agent.phone.includes(searchQuery) ||
       agent.email.toLowerCase().includes(searchQuery.toLowerCase());
 
-    const matchesType = selectedType === "Все" || agent.agentType === agentTypeRepository.getId(selectedType);
+    const matchesType =
+      selectedType === "Все" ||
+      agent.agentType === agentTypeRepository.getId(selectedType);
 
     return matchesSearch && matchesType;
   });
@@ -101,10 +104,7 @@ const App: React.FC = () => {
     setCurrentPage(1);
   };
 
-  const handlePageChange = (
-    _: React.ChangeEvent<unknown>,
-    page: number
-  ) => {
+  const handlePageChange = (_: React.ChangeEvent<unknown>, page: number) => {
     setCurrentPage(page);
   };
 
@@ -114,15 +114,49 @@ const App: React.FC = () => {
   };
 
   const handleSortOrderChange = () => {
-    setSortOrder((prevOrder) => (prevOrder === SortOptions.ASC ? SortOptions.DESC : SortOptions.ASC));
+    setSortOrder((prevOrder) =>
+      prevOrder === SortOptions.ASC ? SortOptions.DESC : SortOptions.ASC
+    );
   };
 
-  const handleCardClick = (agentName: string) => {
+  const handleClickOutside = (event: MouseEvent) => {
+    if (
+      containerRef.current &&
+      !containerRef.current.contains(event.target as Node) &&
+      buttonsRef.current &&
+      !buttonsRef.current.contains(event.target as Node)
+    ) {
+      setSelectedAgents([]);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
+  const containerRef = useRef<HTMLDivElement>(null);
+  const buttonsRef = useRef<HTMLDivElement>(null);
+  const handleCardClick = (agentId: number) => {
     setSelectedAgents((prevSelected) =>
-      prevSelected.includes(agentName)
-        ? prevSelected.filter((name) => name !== agentName)
-        : [...prevSelected, agentName]
+      prevSelected.includes(agentId)
+        ? prevSelected.filter((id) => id !== agentId)
+        : [...prevSelected, agentId]
     );
+  };
+
+  const handleOnChangeAgentClick = () => {
+    console.log("Clicked.");
+  };
+
+  const handleOnChangeAgentsPriorityClick = () => {
+    console.log("Clicked.");
+  };
+
+  const handleOnAddAgentClick = () => {
+    console.log("Clicked.");
   };
 
   return (
@@ -144,48 +178,86 @@ const App: React.FC = () => {
             p={2}
             sx={{ flexGrow: 1 }}
           >
-            <Box display="flex" flexDirection="row" alignItems="center" gap={2}>
-              <Box flexGrow={1}>
-                <SearchField placeholder="Поиск" onSearch={handleSearch} />
-              </Box>
-              <Box display="flex" flexDirection="row" alignItems="center">
-                <Dropdown
-                  options={sortOptions}
-                  onSelect={handleSortSelect}
-                  placeholder="Порядок"
-                  defaultValue={AgentKey.name}
-                />
-                <IconButton
-                  onClick={handleSortOrderChange}
-                  size="small"
-                  sx={{ height: "fit-content" }}
-                >
-                  {sortOrder === SortOptions.ASC ? <ArrowUpward /> : <ArrowDownward />}
-                </IconButton>
-              </Box>
-              <Box>
-                <Dropdown
-                  options={filterOptions}
-                  onSelect={handleTypeSelect}
-                  placeholder="Тип"
-                  defaultValue="Все"
-                />
+            <Box display="flex" flexDirection="column" gap={3}>
+              <SearchField placeholder="Поиск" onSearch={handleSearch} />
+              <Box
+                display="flex"
+                flexDirection="row"
+                alignItems="center"
+                justifyContent="space-between"
+              >
+                <Box display="flex" flexDirection="row" gap={3}>
+                  <Dropdown
+                    options={filterOptions}
+                    onSelect={handleTypeSelect}
+                    placeholder="Тип"
+                    defaultValue="Все"
+                    sx={{ width: 100 }}
+                  />
+                  <Box display="flex" flexDirection="row" alignItems="center">
+                    <Dropdown
+                      options={sortOptions}
+                      onSelect={handleSortSelect}
+                      placeholder="Порядок"
+                      defaultValue={AgentKey.name}
+                      sx={{ width: 200 }}
+                    />
+                    <IconButton
+                      onClick={handleSortOrderChange}
+                      size="small"
+                      sx={{ height: "fit-content" }}
+                    >
+                      {sortOrder === SortOptions.ASC ? (
+                        <ArrowUpward />
+                      ) : (
+                        <ArrowDownward />
+                      )}
+                    </IconButton>
+                  </Box>
+                </Box>
+                <Box ref={buttonsRef} display="flex" flexDirection="row" gap={2}>
+                  {selectedAgents.length > 1 && (
+                    <Button
+                      label="Изменить приоритет"
+                      onClick={handleOnChangeAgentsPriorityClick}
+                      size="large"
+                    ></Button>
+                  )}
+                  {selectedAgents.length === 1 && (
+                  <Button
+                    label="Изменить"
+                    onClick={handleOnChangeAgentClick}
+                    size="large"
+                  ></Button>
+                  )}
+                  {selectedAgents.length === 0 && (
+                  <Button
+                    label="Добавить"
+                    onClick={handleOnAddAgentClick}
+                    size="large"
+                    color="success"
+                  ></Button>
+                  )}
+                </Box>
               </Box>
             </Box>
 
-            <Grid2 container spacing={2} sx={{ flexGrow: 1 }}>
+            <Grid2 ref={containerRef} container spacing={2} sx={{ flexGrow: 1 }}>
               {currentItems.map((agent, index) => (
                 <Grid2 key={index} size={{ xs: 6, sm: 4, md: 3 }}>
                   <AgentCard
                     image={agent.image}
-                    type={agentTypes.get(agent.agentType)?.title || "Неизвестный тип"}
+                    type={
+                      agentTypes.get(agent.agentType)?.title ||
+                      "Неизвестный тип"
+                    }
                     name={agent.name}
                     sales={agent.salesCount}
                     phone={agent.phone}
                     priority={agent.priority}
                     discount={agent.discount}
-                    isSelected={selectedAgents.includes(agent.name)}
-                    onClick={() => handleCardClick(agent.name)}
+                    isSelected={selectedAgents.includes(agent.id)}
+                    onClick={() => handleCardClick(agent.id)}
                   />
                 </Grid2>
               ))}
